bdd6edab1d59f287078c02d37e3e79ef6a2ad7a8
alexandrenone <4lexandrenunes42@gmail.com>
2023-04-05 13:18:49 -0300

    responsivo

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 function carregar(){
     var td1 = document.getElementById('d1')
     var td2 = document.getElementById('d2')
+    var td3 = document.getElementById('d3')
     var img = document.getElementById('imagem')
+    var tp = document.getElementById('tempo')
 
     var data = new Date()
     var hora = data.getHours() 
     var minuto = data.getMinutes()
-
-    var hora = 22
+    var dv3 = td3
     td1.innerHTML = `${hora}:${minuto}`
 
-    if(hora > 0 && hora < 12){
+    if(hora >= 6 && hora < 12){
         img.src = 'manha1.png'
-    }else if(hora > 12 && hora < 18){
+        dv3.innerHTML = 'Manhã'
+        document.body.style.backgroundColor = '#FCE87F'
+        
+    }else if(hora >= 12 && hora < 18){
         img.src = 'tarde.jpg'
-    }else{
+        dv3.innerHTML = 'Tarde'
+        document.body.style.backgroundColor = '#6AD6F3'
+    }else if(hora >= 18 && hora < 24){
         img.src = 'noite.jpg'
+        dv3.innerHTML = 'Noite'
+        document.body.style.backgroundColor = '#332D51'
+    }else{
+        img.src = 'madrugada.jpg'
+        dv3.innerHTML = 'Madrugada'
+        document.body.style.backgroundColor = '#202930'
     }
+
+
 
 
    
